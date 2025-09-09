ac4b015cd3879afa62d4213194e190de93cce9cd
Kip <jeretech.jeremy@gmail.com>
2025-09-09 22:44:03 +0800

    started building imgui also made an icon!

--- window_main.cpp
+++ window_main.cpp
@@ -7,6 +7,10 @@
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
 
+#define STB_IMAGE_IMPLEMENTATION
+#define STBI_FAILURE_USERMSG
+#include "external/stb_image.h"
+
 void error_callback(int error, const char* description)
 {
 	std::cerr<<"Error: " << description << '\n';
@@ -14,9 +18,11 @@ void error_callback(int error, const char* description)
 
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, GLFW_TRUE);
+	// if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	// 	glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
+
+void RenderInterface();
 
 int main(int argc, char* argv[])
 {
@@ -28,13 +34,21 @@ int main(int argc, char* argv[])
 		exit(EXIT_FAILURE);
 	}
 
-	GLFWwindow* window = glfwCreateWindow(1400, 800, "Pathfinder", NULL, NULL);
+	// Window creation
+	GLFWwindow* window = glfwCreateWindow(1400, 800, "Raytrack", NULL, NULL);
+
 	if (!window)
 	{
 		std::cerr << "Failed to create Window!" << '\n';
 		glfwTerminate();
 		exit(EXIT_FAILURE);
 	}
+
+	// icon :3
+	GLFWimage images[1];
+	images[0].pixels = stbi_load("icon.png", &images[0].width, &images[0].height, 0, 4);
+	glfwSetWindowIcon(window, 1, images);
+	stbi_image_free(images[0].pixels);
 
 	glfwMakeContextCurrent(window);
 	gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
@@ -63,18 +77,17 @@ int main(int argc, char* argv[])
 		ImGui_ImplOpenGL3_NewFrame();
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
-		ImGui::ShowDemoWindow();
+
+		// ImGui::ShowDemoWindow();
 
 		// Window loop
 		int width, height;
 		glfwGetFramebufferSize(window, &width, &height);
-		// const float ratio = width / (float) height;
 
-		// std::clog << "w: " << width << " h: " << height << '\n';
 
 		glViewport(0, 0, width, height);
 
-		// double time = glfwGetTime();
+		RenderInterface();
 
 		// Display
 		glClear(GL_COLOR_BUFFER_BIT); // or not :3
@@ -91,4 +104,51 @@ int main(int argc, char* argv[])
 
 	glfwDestroyWindow(window);
 	glfwTerminate();
+}
+
+
+void RenderInterface()
+{
+
+	static bool v_enabled = true;
+	static bool v2_enabled = true;
+	if (ImGui::BeginMainMenuBar())
+	{
+		if (ImGui::BeginMenu("Windows"))
+		{
+			ImGui::MenuItem("Viewport", "", &v_enabled);
+			ImGui::MenuItem("Camera Settings", "", &v_enabled);
+			ImGui::MenuItem("Scene Hierarchy", "", &v_enabled);
+			ImGui::MenuItem("Inspector", "", &v_enabled);
+			ImGui::MenuItem("Geometries", "", &v_enabled);
+			ImGui::MenuItem("Textures", "", &v_enabled);
+			ImGui::MenuItem("Materials", "", &v_enabled);
+
+			ImGui::EndMenu();
+		}
+
+		ImGui::EndMainMenuBar();
+	}
+
+	ImGui::SetNextWindowSize(ImVec2(600, 400), ImGuiCond_FirstUseEver);
+	if (!ImGui::Begin("Viewport", (bool*)v_enabled, 0))
+	{
+		// Early out if the window is collapsed, as an optimization.
+		ImGui::End();
+		return;
+	}
+	ImGui::Text("Go to Menu > Windows! 800x600");
+
+	ImGui::End();
+
+	ImGui::SetNextWindowSize(ImVec2(600, 400), ImGuiCond_FirstUseEver);
+	if (!ImGui::Begin("Viewport 2", (bool*)v2_enabled, 0))
+	{
+		// Early out if the window is collapsed, as an optimization.
+		ImGui::End();
+		return;
+	}
+	ImGui::Text("2! Go to Menu > Windows! 800x600");
+
+	ImGui::End();
 }
